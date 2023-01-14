a067d1ab3c0af3c25dfc186a124f30b6d5cbfa25
thayron matos <thayronf3@gmail.com>
2023-01-14 14:53:43 -0300

    feat: adds contracts to provider typedocs

--- src/type_docs/type_docs.providers.ts
+++ src/type_docs/type_docs.providers.ts
@@ -1,7 +1,7 @@
-import { DataSource } from 'typeorm';
+import { IProvider, DataSource } from 'src/contracts/providers';
 import { TypeDoc } from './entities/type_doc.entity';
 
-export const typeDocsProviders = [
+export const typeDocsProviders: IProvider<TypeDoc>[] = [
   {
     provide: 'TYPE_DOCS_REPOSITORY',
     useFactory: (dataSource: DataSource) => dataSource.getRepository(TypeDoc),
